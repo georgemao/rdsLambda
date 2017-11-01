5e03fe3e1b7c4d3ea0035a27510ab23d3148c1a3
georgmao <georgmao@amazon.com>
2017-11-01 09:58:33 -0400

    adding py example

---
+++ myrdstest.py
@@ -0,0 +1,39 @@
+import mysql.connector
+import boto3
+import os
+
+RDS_HOST = 'myaurorards.cvbjqjhsfcqp.us-west-2.rds.amazonaws.com'
+RDS_USER = 'rds_user'
+RDS_REGION = 'us-west-2'
+
+rds = boto3.client('rds')
+
+rds = boto3.client(
+    'rds',
+    aws_access_key_id='xxxxxx',
+    aws_secret_access_key='yyyyyyyy'
+)
+
+
+def handler(event, context):
+    password = rds.generate_db_auth_token(
+        DBHostname=RDS_HOST,
+        Port=3306,
+        DBUsername=RDS_USER
+    )
+
+    conn = mysql.connector.connect(
+        user=RDS_USER,
+        password=password,
+        host=RDS_HOST,
+        database='my_db',
+        charset='utf8',
+        ssl_verify_cert=True,
+        ssl_ca='./rds-combined-ca-bundle.pem'
+    )
+
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Employees')
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
